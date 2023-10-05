106c01dd9031ab2f5296e893340ac7ec1eaee626
SmeeBoi <sameer.desai.boston@gmail.com>
2023-10-05 14:17:17 -0400

    added sample calendar and dependencies in package.json

--- src/frontend/main.jsx
+++ src/frontend/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Calendar from './screens/Calendar.jsx'
 import './css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Calendar/>
   </React.StrictMode>,
 )
