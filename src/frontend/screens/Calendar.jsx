5ad590952a9ac6f1c34713e02dc671c686315ed7
SmeeBoi <sameer.desai.boston@gmail.com>
2023-10-05 14:19:19 -0400

    uses calendar.css

--- src/frontend/screens/Calendar.jsx
+++ src/frontend/screens/Calendar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import Calendar from "react-calendar"
 import "react-calendar/dist/Calendar.css"
 
-// import "./Sample.css" 
+import "../css/calendar.css" 
 
 export default function Sample() {
   const [value, onChange] = useState(new Date())
